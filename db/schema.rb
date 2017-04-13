6ab9b057993c9a975c50e377a11f997096b711b1
VatsalaSingh <vatsalarajput@yahoo.com>
2017-04-13 16:40:25 +0530

    Tweets model created & user_id foreign key added

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170413094516) do
+ActiveRecord::Schema.define(version: 20170413110324) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
+
+  add_index "tweets", ["user_id"], name: "index_tweets_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "email", null: false
-    t.string   "password", null: false
+    t.string   "email"
+    t.string   "password"
   end
 
 end
